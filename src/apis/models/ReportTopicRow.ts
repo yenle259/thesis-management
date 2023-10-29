b5366b87cc08411772db9b4d166c894f42221762
yenle259 <lebuihongyen@gmail.com>
2023-10-29 18:03:30 +0700

    feat: export report topic

---
+++ src/apis/models/ReportTopicRow.ts
@@ -0,0 +1,8 @@
+export interface ReportTopicRow {
+  mscb: string;
+  gv: string;
+  mssv: string;
+  sv: string;
+  ten_de_tai: string;
+  bao_cao: string;
+}
--- src/views/manage.topic.vue
+++ src/views/manage.topic.vue
@@ -5,7 +5,13 @@
       :subTitle="'Danh sách đề tài đã được duyệt'"
     >
       <template v-slot:action>
-        <v-btn variant="tonal" color="blue">Xuất DS đề tài</v-btn>
+        <v-btn
+          v-if="reports"
+          variant="tonal"
+          color="blue"
+          @click="handleExportReports(reports)"
+          >Xuất DS đề tài</v-btn
+        >
       </template>
       <template v-slot:content>
         <ManageReportTopic :reports="reports ?? []" />
@@ -17,6 +23,8 @@
 <script setup lang="ts">
 import API from "@/apis/helpers/axiosBaseConfig";
 import { ReportTopic } from "@/apis/models/ReportTopic";
+import { ReportTopicRow } from "@/apis/models/ReportTopicRow";
+import { utils, writeFileXLSX } from "xlsx";
 
 useTitle("QLĐT - Quản lý đề tài được duyệt");
 
@@ -34,5 +42,27 @@ const getReports = async () => {
 
 getReports();
 
-console.log(reports);
+const createDataToExport = (reports: ReportTopic[]) => {
+  return reports.map(({ pi, topic, student, isReport }) => ({
+    "Mã số giảng viên": pi.userId,
+    "Họ tên Giảng viên": pi.name,
+    "Mã số sinh viên": student.userId,
+    "Họ tên sinh viên": student.name,
+    "Tên đề tài": topic.name,
+    "Phân loại đề tài": getTopicTypeName(topic.type),
+    "Xác nhận báo cáo": isReport ? "Có" : "Chưa",
+  }));
+};
+
+const handleExportReports = (reports: ReportTopic[]) => {
+
+  const data = createDataToExport(reports);
+  
+  const ws = utils.json_to_sheet(data);
+  /* create workbook and append worksheet */
+  const wb = utils.book_new();
+  utils.book_append_sheet(wb, ws, "Data");
+  /* export to XLSX */
+  writeFileXLSX(wb, "Danh sach huong dan de tai.xlsx");
+};
 </script>
